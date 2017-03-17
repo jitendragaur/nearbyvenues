0c51da654f5c8c7402b7582d69b15df023272068
Jitendra Gaur <jitendra.gaur@me.com>
2017-03-17 23:19:19 +0530

    - Initial commit

---
+++ nearByVenues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nearByVenues
+//
+//  Created by Jitendra Gaur on 3/17/17.
+//  Copyright © 2017 Jitendra Gaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

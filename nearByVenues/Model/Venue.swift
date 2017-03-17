8c8ce5c9778f799770c9690ce6bcc71f541e83a3
Jitendra Gaur <jitendra.gaur@me.com>
2017-03-17 23:36:23 +0530

    - Setup folder structure

---
+++ nearByVenues/Model/Venue.swift
@@ -0,0 +1,13 @@
+//
+//  Venue.swift
+//  nearByVenues
+//
+//  Created by Jitendra Gaur on 3/17/17.
+//  Copyright © 2017 Jitendra Gaur. All rights reserved.
+//
+
+import Foundation
+
+struct Venue {
+    
+}
